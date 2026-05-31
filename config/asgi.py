33f04e043827ac1bc03f05afb3d625639a48f65d
김상진 <ltedtid@gmail.com>
2026-05-31 17:31:59 +0900

    fix: load django before chat routing

--- config/asgi.py
+++ config/asgi.py
@@ -10,15 +10,17 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
+
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = ProtocolTypeRouter({
-	'http': get_asgi_application(),
+	'http': django_asgi_app,
 	'websocket': AuthMiddlewareStack(
 		URLRouter(
 			chat.routing.websocket_urlpatterns
